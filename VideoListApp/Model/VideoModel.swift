95ee127318eb3874ce297a4bbd5462495e6b765c
mert.koksal <mert.koksal@ordulu.com>
2022-06-24 15:52:14 +0300

    model completed

--- VideoListApp/Model/VideoModel.swift
+++ VideoListApp/Model/VideoModel.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct VideoModel: Decodable {
+    var vidoeId: String {
+        return id
+    }
+    let id: String
+    let title: String
+    let hlsURL: String
+    let fullURL: String
+    let description: String
+    let publishedAt: String
+    let author: Author
+}
+
+struct Author: Decodable {
+    let id: String
+    let name: String
+}
