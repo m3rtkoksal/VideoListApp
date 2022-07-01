3568bef038b34aef07ae6427e98cd6447672ad62
mert.koksal <mert.koksal@ordulu.com>
2022-07-01 10:31:02 +0300

    avKit

--- VideoListApp/VideoDetail/VideoDetailViewController.swift
+++ VideoListApp/VideoDetail/VideoDetailViewController.swift
@@ -2,22 +2,20 @@
 //  VideoDetailViewController.swift
 //  VideoListApp
 //
-//  Created by Mert Köksal on 27.06.2022.
+//  Created by Mert Köksal on 1.07.2022.
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class VideoDetailViewController: UIViewController {
-    var selectedVideo: VideoModel!
-    
-    lazy var spinner: UIActivityIndicatorView = {
-        let activityIndicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
-        activityIndicator.color = UIColor.gray
-        activityIndicator.startAnimating()
-        activityIndicator.hidesWhenStopped = true
-        return activityIndicator
-    }()
-    
-    
-    
+
+    @IBOutlet weak var vmPlayer: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
 }
